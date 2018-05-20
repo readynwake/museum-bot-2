f5485a07ac5993cb153315a8bf6f3c9da9e324e8
N4D1A <35233687+N4D1A@users.noreply.github.com>
2018-05-20 19:28:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ var app = express(); //
 //// var server = require('http').createServer(app);
 //// var port = process.argv[2] || 5000;
 
-app.listen(process.env.PORT || 5000, function(){
-  console.log('Server listening');
-  ////console.log('Server listening at port %d', port);
-  ////console.log('Server dirname : ', __dirname);
+app.listen(process.env.PORT || 5000, function () {
+    console.log('Server listening');
+    ////console.log('Server listening at port %d', port);
+    ////console.log('Server dirname : ', __dirname);
 });
 
 var bodyParser = require('body-parser'); //
 //// app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); //
-app.use(bodyParser.urlencoded({extended: true})); //
+app.use(bodyParser.urlencoded({ extended: true })); //
 
 
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('info.json', 'utf8'));
 
-console.log('Answer: '+ obj['금동신발']['재질']); //// debugging
+console.log('Answer: ' + obj['금동신발']['재질']); //// debugging
 
 // app.post('/', function(req, res){
 //   var speech = 
@@ -35,31 +35,70 @@ console.log('Answer: '+ obj['금동신발']['재질']); //// debugging
 //     source: "museum-bot"
 //   });
 // });
-     
-      
+
+
 //app.all('/', function(req, res){
 //app.get('/', function(req, res){
 
-app.post('/', function(req, res){
-  console.log('req: \n' + JSON.stringify(req.body));
-  //var item = req.body.result.parameters['item'];
-  var item = req.body.queryResult.parameters['item'];
-  var material = req.body.queryResult.parameters['material'];
-  var size = req.body.queryResult.parameters['size'];
-  var discover_time = req.body.queryResult.parameters['discover_time'];
+app.post('/', function (request, response) {
+    console.log('request: \n' + JSON.stringify(request.body));
+    //var item = req.body.result.parameters['item'];
+    var item = request.body.queryResult.parameters['item'];
+    var material = request.body.queryResult.parameters['material'];
+    var size = request.body.queryResult.parameters['size'];
+    var discover_time = request.body.queryResult.parameters['discover_time'];
 
-  var output = "답변드립니다. " + obj[item][size];
+    let action = (request.body.queryResult.action) ? request.body.queryResult.action ; "default';
 
-  ////res.setHeader('Content-Type', 'application/json');
-  ////res.send(JSON.stringify({'speech': output, 'displayText': output}));
-  return res.json({
-    fulfillmentText: output,
-    source: "museum-bot"
-  });
+
+    const actionHandlers = {
+        'get.item.name': () => {
+            let responseToUser = { fulfillmentText: '답변입니다.' + obj[item][name] };
+            sendResponse(responseToUser);
+        },
+        'get.item.size': () => {
+            let responseToUser = { fulfillmentText: '답변입니다.' + obj[item][size] };
+            sendResponse(responseToUser);
+        },
+        'default': () => {
+            let responseToUser = { fulfillmentText: '디폴트 액션 처리' };
+            sendResponse(responseToUser);
+        }
+    };
+
+    if (!actionHandlers[action]) {
+         action = 'default';
+    }
+
+    actionHandlers[action]();
+
+    function sendResponse(responseToUser) {
+        if (typeof responseToUser === 'string') {
+            let responseJson = { fulfillmentText: responseToUser };
+            response.json(responseJson);
+        }
+        else {
+            let responseJson = {};
+            responseJson.fulfillmentText = responseToUser.fulfillmentText;
+            if (responseToUser.fulfillmentMessages) {
+                responseJson.fulfillmentMessages = responseToUser.fulfillmentMessages;
+            }
+            if (responseToUser.outputContexts) {
+                responseJson.outputContexts = responseToUser.outputContexts;
+            }
+            response.json(responseJson);
+        }
+    }
 });
 
-/////////////////////////////////////////
-//var path = require('path');
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-/////////////////////////////////////////
+//var output = "답변드립니다. " + obj[item][size];
+
+////res.setHeader('Content-Type', 'application/json');
+////res.send(JSON.stringify({'speech': output, 'displayText': output}));
+
+
+// return res.json({
+//     fulfillmentText: output,
+//     source: "museum-bot"
+// });
+// });

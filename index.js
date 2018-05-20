095e0801e4d708679c986cec5f42055580a02c6b
N4D1A <35233687+N4D1A@users.noreply.github.com>
2018-05-20 11:29:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -39,28 +39,4 @@ app.get('/', function(req, res){
 var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', { message: err.message,
-    error: {}
-  });
-});
 /////////////////////////////////////////

34f5e08869700c582ce97fe14ebe5155a40649b8
wjkang619 <39460813+wjkang619@users.noreply.github.com>
2018-06-07 10:51:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 var express = require('express'); //
 var app = express(); //
-//// var server = require('http').createServer(app);
-//// var port = process.argv[2] || 5000;
+
 
 app.listen(process.env.PORT || 5000, function () {
     console.log('Server listening');
@@ -22,57 +21,25 @@ var obj = JSON.parse(fs.readFileSync('info.json', 'utf8'));
 
 console.log('Answer: ' + obj['금동신발']['재질']); //// debugging
 
-// app.post('/', function(req, res){
-//   var speech = 
-//       req.body.queryResult &&
-//       req.body.queryResult.parameters &&
-//       req.body.queryresult.parameters.echoText  
-//       ? req.body.queryresult.parameters.echoText  
-//       : "Seems like some problem. Speak again.";
-//   return res.json({
-//     speech: speech,
-//     displayText: speech,
-//     source: "museum-bot"
-//   });
-// });
-
-
-//app.all('/', function(req, res){
-//app.get('/', function(req, res){
-
 app.post('/', function (request, response) {
     console.log('request: \n' + JSON.stringify(request.body));
     //var item = req.body.result.parameters['item'];
-    var item = request.body.queryResult.parameters['item'];
-    var name = request.body.queryResult.parameters['name'];
-    var material = request.body.queryResult.parameters['material'];
-    var size = request.body.queryResult.parameters['size'];
-    var era = request.body.queryResult.parameters['era'];
+    var what = request.body.queryResult.parameters['what'];
+    var who = request.body.queryResult.parameters['who'];
+    var when = request.body.queryResult.parameters['when'];
+    var where = request.body.queryResult.parameters['where'];
+    var how = request.body.queryResult.parameters['how'];
+    var why = request.body.queryResult.parameters['why'];
 
     let action = (request.body.queryResult.action) ? request.body.queryResult.action: 'default';
 
 
     const actionHandlers = {
-        'get.item': () => {
-            let responseToUser = { fulfillmentText: item+'에 대해 알려드릴게요. 무엇이 궁금하신가요?'};
+        'what.how': () => {
+            let responseToUser = { fulfillmentText: '답변드립니다. ' + obj[what][how]};
             sendResponse(responseToUser);
         },
-        'get.item.name': () => {
-            let responseToUser = { fulfillmentText: '답변드립니다. ' + obj[item][name] };
-            sendResponse(responseToUser);
-        },
-        'get.item.size': () => {
-            let responseToUser = { fulfillmentText: '답변드립니다. ' + obj[item][size] };
-            sendResponse(responseToUser);
-        },
-        'get.item.material': () => {
-            let responseToUser = { fulfillmentText: '답변드립니다. ' + obj[item][material] };
-            sendResponse(responseToUser);
-        },
-        'get.item.era': () => {
-            let responseToUser = { fulfillmentText: '답변드립니다. ' + obj[item][era] };
-            sendResponse(responseToUser);
-        },
+   
         'default': () => {
             let responseToUser = { fulfillmentText: '아직 입력되지 않은 질문입니다.' };
             sendResponse(responseToUser);
@@ -104,14 +71,3 @@ app.post('/', function (request, response) {
     }
 });
 
-//var output = "답변드립니다. " + obj[item][size];
-
-////res.setHeader('Content-Type', 'application/json');
-////res.send(JSON.stringify({'speech': output, 'displayText': output}));
-
-
-// return res.json({
-//     fulfillmentText: output,
-//     source: "museum-bot"
-// });
-// });

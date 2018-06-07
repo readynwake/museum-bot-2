644549c7f7a501baa2f5898cd90dcd2599c2272f
wjkang619 <39460813+wjkang619@users.noreply.github.com>
2018-06-07 12:35:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 var express = require('express'); //
 var app = express(); //
-
+//// var server = require('http').createServer(app);
+//// var port = process.argv[2] || 5000;
 
 app.listen(process.env.PORT || 5000, function () {
     console.log('Server listening');
@@ -19,7 +20,25 @@ app.use(bodyParser.urlencoded({ extended: true })); //
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('info.json', 'utf8'));
 
-console.log('Answer: ' + obj['금동신발']['재질']); //// debugging
+console.log('Answer: ' + obj['경주봉황대']['정의']); //// debugging
+
+// app.post('/', function(req, res){
+//   var speech = 
+//       req.body.queryResult &&
+//       req.body.queryResult.parameters &&
+//       req.body.queryresult.parameters.echoText  
+//       ? req.body.queryresult.parameters.echoText  
+//       : "Seems like some problem. Speak again.";
+//   return res.json({
+//     speech: speech,
+//     displayText: speech,
+//     source: "museum-bot"
+//   });
+// });
+
+
+//app.all('/', function(req, res){
+//app.get('/', function(req, res){
 
 app.post('/', function (request, response) {
     console.log('request: \n' + JSON.stringify(request.body));

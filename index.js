45dec63586e6aba6434de0008e0a6c1231402a09
wjkang619 <39460813+wjkang619@users.noreply.github.com>
2018-06-09 13:03:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
 
 var express = require('express'); //
 var app = express(); //
-//// var server = require('http').createServer(app);
-//// var port = process.argv[2] || 5000;
+
 
 app.listen(process.env.PORT || 5000, function () {
     console.log('Server listening');
-    ////console.log('Server listening at port %d', port);
-    ////console.log('Server dirname : ', __dirname);
+
 });
 
 var bodyParser = require('body-parser'); //
-//// app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.json()); //
 app.use(bodyParser.urlencoded({ extended: true })); //
 
@@ -21,25 +19,6 @@ var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('info.json', 'utf8'));
 
 console.log('Answer: ' + obj['경주봉황대']['정의']); //// debugging
-
-// app.post('/', function(req, res){
-//   var speech = 
-//       req.body.queryResult &&
-//       req.body.queryResult.parameters &&
-//       req.body.queryresult.parameters.echoText  
-//       ? req.body.queryresult.parameters.echoText  
-//       : "Seems like some problem. Speak again.";
-//   return res.json({
-//     speech: speech,
-//     displayText: speech,
-//     source: "museum-bot"
-//   });
-// });
-
-
-//app.all('/', function(req, res){
-//app.get('/', function(req, res){
-
 
 app.post('/', function (request, response) {
     console.log('request: \n' + JSON.stringify(request.body));
